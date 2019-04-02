aa7e37ca793210503856ae77c705768316891dfe
ZouBingyang <282379425@qq.com>
2019-04-02 20:42:44 +0800

    4/2

--- src/xuexi/Hello.java
+++ src/xuexi/Hello.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 
 public class Hello {
 	public static void main(String[] args) {
-		int[] a = new int[10];
-		for (int i = 0; i < 10; i++)
-			a[i] = i;
-		System.out.println(Arrays.toString(a));
-		for (int element : a)
-			System.out.print(element * element + "\t");
+		System.out.println("commit 4/2");
 	}
 }
